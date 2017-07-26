1c9e8863a3959022afb738f08b1f5ad2f367c326
Attila Domokos <adomokos@gmail.com>
2017-07-26 10:28:47 +0200

    All examples should have a solution

--- test/01_extracting_portion_of_list_Spec.hs
+++ test/01_extracting_portion_of_list_Spec.hs
@@ -7,19 +7,18 @@ main = hspec $ do
     describe "Extracting portion of list" $ do
         it "takes elements from a list" $ do
             take 7 ['a'..'z'] `shouldBe` "abcdefg"
-            pendingWith "Please fix it below"
-            [1..5] `shouldBe` [1,2,3]
-            {- (enumFrom 10) `shouldBe` [10,11,12,13,14] -}
-        {- it "can drop elements from a list" $ do -}
-            {- [1..10] `shouldBe` [6,7,8,9,10] -}
-            {- ['a'..'g'] `shouldBe` "defg" -}
-            {- (enumFromTo 10 20) `shouldBe` [16,17,18,19,20] -}
-        {- it "can split a collection" $ do -}
-            {- [1..10] `shouldBe` ([1,2,3,4,5],[6,7,8,9,10]) -}
-        {- it "can take while" $ do -}
-            {- [1..10] `shouldBe` [1,2] -}
-            {- (enumFromTo 5 15) `shouldBe` [5,6,7] -}
-            {- "abracadabra" `shouldBe` "a" -}
-        {- it "can drop while" $ do -}
-            {- [1..10] `shouldBe` [5,6,7,8,9,10] -}
-            {- "abracadabra" `shouldBe` "bracadabra" -}
+            take 3 [1..5] `shouldBe` [1,2,3]
+            take 5 (enumFrom 10) `shouldBe` [10,11,12,13,14]
+        it "can drop elements from a list" $ do
+            drop 5 [1..10] `shouldBe` [6,7,8,9,10]
+            drop 3 ['a'..'g'] `shouldBe` "defg"
+            drop 6 (enumFromTo 10 20) `shouldBe` [16,17,18,19,20]
+        it "can split a collection" $ do
+            splitAt 5 [1..10] `shouldBe` ([1,2,3,4,5],[6,7,8,9,10])
+        it "can take while" $ do
+            takeWhile (<3) [1..10] `shouldBe` [1,2]
+            takeWhile (<8) (enumFromTo 5 15) `shouldBe` [5,6,7]
+            takeWhile (=='a') "abracadabra" `shouldBe` "a"
+        it "can drop while" $ do
+            dropWhile (<5) [1..10] `shouldBe` [5,6,7,8,9,10]
+            dropWhile (=='a') "abracadabra" `shouldBe` "bracadabra"
